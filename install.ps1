dbd37a6dfbc585d17af04e9157d2ed1b98f26f48
Scharly Ochoa <scharly.ochoa@tranzact.net>
2025-07-23 10:28:39 -0500

    added default configs

---
+++ install.ps1
@@ -0,0 +1,17 @@
+﻿# Check if GlazeWM is already installed
+$glaze = winget list --name GlazeWM | Select-String "GlazeWM"
+if ($glaze) {
+    Write-Host "GlazeWM is already installed."
+} else {
+    Write-Host "Installing GlazeWM..."
+    winget install GlazeWM
+}
+
+# Check if GlazeWM is already running
+$process = Get-Process -Name "glazewm" -ErrorAction SilentlyContinue
+if ($process) {
+    Write-Host "GlazeWM is already running."
+} else {
+    Write-Host "Starting GlazeWM..."
+    Start-Process "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
+}
